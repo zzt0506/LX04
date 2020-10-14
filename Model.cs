3b276d5912a048f4e802e4dc42b39227795b3803
zzt0506 <71053203+zzt0506@users.noreply.github.com>
2020-10-14 19:15:05 +0800

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Model
+{
+    public class UserInfo
+    {
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public string qq { get; set; }
+        public string email { get; set; }
+        public string type { get; set; }
+        public string userImg { get; set; }
+    }
+    public class UserInfoNo : UserInfo
+    {
+        public int num { get; set; }
+    }
+    public class Page
+    {
+        public int pageIndex { get; set; }
+        public int pageSize { get; set; }
+    }
+    public class Activity
+    {
+        public int activityId { get; set; }
+
+        public string activityName { get; set; }
+
+        public DateTime endTime { get; set; }
+
+        public string activityPicture { get; set; }
+
+        public string activityIntroduction {get;set;}
+
+        public string Summary { get; set; }
+        public string activityVerify { get; set; }
+
+        public string activityStatus { get; set; }
+
+        public string userName { get; set; }
+        public string recomMend { get; set; }
+        public DateTime recommendTime { get; set; }
+    }
+    public class WorkInfo
+    {
+        public int workId { get; set; }
+        public string workName { get; set; }
+        public string workPicture { get; set; }
+
+        public DateTime uploadTime { get; set; }
+
+        public string workIntroduction { get; set; }
+
+        public string workVerify { get; set; }
+
+        public string userName { get; set; }
+        public int activityId { get; set; }
+        public string recomMend { get; set; }
+        public DateTime recommendTime { get; set; }
+    }
+    public class Comment
+    {
+        public int commentId { get; set; }
+        public int workID { get; set; }
+        public string userName { get; set; }
+        public string commentContent { get; set; }
+
+        public DateTime commentTime { get; set; }
+    }
+    
+    public class WorkFindPage : Page
+    {
+        public string workName { get; set; }
+    }
+    public class WorkMyPage : Page
+    {
+        public string userName { get; set; }
+    }
+    public class CommentPage : Page
+    {
+        public string workId { get; set; }
+    }
+    public class ActivityNo:Activity
+    {
+        public int num { get; set; }
+    }
+    public class ActivityName
+    {
+        public int activityId { get; set; }
+
+        public string activityName { get; set; }
+    }
+    public class WorkInfoNo :WorkInfo
+    {
+        public int num { get; set; }
+        public string activityName { get; set; }
+    }
+    public class CommentNo :Comment
+    {
+        public int num { get; set; }
+        public string workName { get; set; }
+    }
+}
---
+++ UploadController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Service
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public UploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpPost]
+        public ActionResult  Post(List<IFormFile> files)
+        {
+            var path = Path.Combine(_hostingEnvironment.WebRootPath, "img", "Editor");
+            string fName = Guid.NewGuid().ToString().Replace("-", "");
+            var fileName = $"{path}/{fName}";
+            try
+            {
+                var ext = DAL.Upload.Instance.UpImg(files[0], fileName);
+                if (ext == null)
+                    return Json(Result.Err("请上传图片文件"));
+                else
+                {
+                    var file = $"https://{HttpContext.Request.Host.Value}/img/Editor/{fName}{ext}";
+                    return Json(Result.Ok("上传成功", file));
+                }
+            }
+            catch(Exception ex)
+            {
+                return Json(Result.Err(ex.Message));
+            }
+        }
+    }
+}
